af35f3b3cb713b12a96faf24cfdaf8b57a165475
Angel-Uriel-Castillo <143562793+Angel-Uriel-Castillo@users.noreply.github.com>
2023-09-19 12:55:36 -0700

    Add files via upload

---
+++ EVA1_9_INDENTIFICADOR/src/main/java/com/mycompany/eva1_9_indentificador/EVA1_9_INDENTIFICADOR.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_9_indentificador;
+
+/**
+ *
+ * @author uriel
+ */
+public class EVA1_9_INDENTIFICADOR {
+
+    public static void main(String[] args) {
+        //int 1nombre, nombre no valido, empieza con un numero
+        int $nombre;
+        int _nombre;
+        int nombre1;
+        int nombre;
+        int Nombre;
+        int nombre_dato;
+        //int nombre; la variable ya existe previamente
+        
+        //int public; no podemos usar public, es palabra reservada (keyword)
+        int Public;
+        double zbvsbdcibica;
+    }
+}
